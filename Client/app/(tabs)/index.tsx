9e0e9c038ea79114a5a68a3e62504c7a62157318
Amirreza <rezaamir731@gmail.com>
2026-06-25 22:40:22 +0330

    feat: tabs

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import {StyleSheet} from "react-native";
-import {ThemedView} from "@/components/ui/themed-view";
+import { StyleSheet } from "react-native";
+import { ThemedView } from "@/components/ui/themed-view";
 import TemperatureMonitor from "@/components/Sections/TemperatureMonitor";
 import Modes from "@/components/Sections/Modes";
 import ControlButtons from "@/components/Sections/ControlButtons";
@@ -8,18 +8,16 @@ import useLocalStateStore from "@/stores/localStateStore";
 import { useStatus } from "@/hooks/useStatus";
 
 export default function HomeScreen() {
-    const mode = useLocalStateStore(s => s.mode);
-  const {status } = useStatus();
-    return (
-        <ThemedView>
-            <TemperatureMonitor/>
-            <Modes/>
-            {mode === "AUTO" &&
-                <TemperatureSelector/>
-            }
-            <ControlButtons/>
-        </ThemedView>
-    );
+  const mode = useLocalStateStore((s) => s.mode);
+  const { data } = useStatus();
+  return (
+    <ThemedView>
+      <TemperatureMonitor />
+      <Modes />
+      {mode === "AUTO" && <TemperatureSelector />}
+      <ControlButtons />
+    </ThemedView>
+  );
 }
 
 const styles = StyleSheet.create({});
